f6099cffaefdb77489f65ef28c20e5acf5d4d058
pietro2356 <pietro.rocchio@gmail.com>
2018-10-19 19:40:28 +0200

    Add variable example code.

---
+++ C-Costruct-Example/Variable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int integer = 5;
+double virgola = 1,5;
+float floating = 512.56;
+char c = 'c';
+
+int main(){
+	printf("Variabile intera >> %d\r\n", integer);
+	printf("Variabile a virgola mobile >> %d\r\n", virgola);
+	printf("Variabile a virgola mobile >> %d\r\n", floating);
+	printf("Variabile di tipo carattere >> %d\r\n", c);
+	
+	return 0;
+}
