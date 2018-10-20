ac8b96c0775e096a3a3f202fb853f705b0c62fd2
pietro2356 <pietro.rocchio@gmail.com>
2018-10-20 13:01:20 +0200

    Add comment for logical variable.

--- C-Costruct-Example/Variable.c
+++ C-Costruct-Example/Variable.c
@@ -5,6 +5,9 @@ double virgola = 1.5;
 float floating = 512.56;
 char c = 'c';
 
+//In C, non esistono variabili logiche;
+//Quindi falso == 0;
+
 int main(){
 	printf("Variabile intera >> %d\r\n", integer);
 	printf("Variabile a virgola mobile >> %f\r\n", virgola);
